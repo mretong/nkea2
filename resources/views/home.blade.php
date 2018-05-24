3feff6c189f3715cd40b2e6e7ce18cc3f06eb580
Mohd Akmal Alias <mretong@gmail.com>
2018-05-24 08:59:52 +0800

    edit login page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">SRPT - NKEA</div>
-                    <img src="{{ asset('img/logo.png') }}">
+                        <img src="{{ asset('img/logo.png') }}">
                     <font color="white">Version : 1.4.02.170518</font>
                 <div class="card-body">
                     @if (session('status'))
